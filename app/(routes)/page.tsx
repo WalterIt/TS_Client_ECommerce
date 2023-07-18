ac264708dedace163fd9abc0c59d31a9c8330f52
WalterIt <whiruba@gmail.com>
2023-07-18 14:37:34 -0300

    Featured Products - Billboard, ProductList, ProductCard Components. GetProducts Functionality

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,8 +1,24 @@
+import getBillboard from "@/actions/GetBillboard";
+import getProducts from "@/actions/GetProducts";
+import Billboard from "@/components/ui/Billboard";
+import Container from "@/components/ui/Container";
+import ProductList from "@/components/ui/ProductList";
 import React from "react";
-export default function HomePage() {
+
+export const revalidate = 0;
+
+export default async function HomePage() {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("bfe23158-36ff-48a6-94d3-d467ecfa16f6");
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-6xl">Hello World</h1>
-    </div>
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
   );
 }
